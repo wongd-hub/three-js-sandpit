46680de3bbda39151a87be238d271b75599a544c
Darren Wong <email@darrenwong.net>
2022-01-17 19:23:00 +1100

    Updated camera in particle field

--- components/ParticleNet.js
+++ components/ParticleNet.js
@@ -1,10 +1,13 @@
 // Adapted from this sandbox https://codesandbox.io/s/lt473?file=/src/App.js
-import React, { useMemo, useRef, Suspense } from "react"
+import React, { useMemo, useRef, Suspense, useEffect } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls, Loader } from "@react-three/drei"
+import * as THREE from 'three'
 import './effects/dotmaterial'
 
 export function Particles({ rows, cols }) {
+    const lookat = new THREE.Vector3(0, -8, 0)
+
     const [coords, sizes, colours] = useMemo(() => {
       const initialCoords = []
       const initialSizes = []
@@ -34,10 +37,12 @@ export function Particles({ rows, cols }) {
     useFrame((state) => {
         geom.current.material.uniforms.time.value = state.clock.elapsedTime
         geom.current.geometry.verticesNeedUpdate = true
+
+        state.camera.lookAt(lookat)
     })
   
     return (
-      <points ref={geom} position={[0, 1, 0]} rotation={[-Math.PI / 4, 0, Math.PI / 6]}>
+      <points ref={geom} position={[0, 0, 0]} rotation={[-Math.PI / 4, 0, Math.PI / 6]}>
         <bufferGeometry attach="geometry">
           <bufferAttribute attachObject={["attributes", "position"]} count={coords.length / 3} array={coords} itemSize={3} />
           <bufferAttribute attachObject={["attributes", "size"]} count={sizes.length} array={sizes} itemSize={1} />
@@ -51,11 +56,12 @@ export function Particles({ rows, cols }) {
 
 export default function ParticleField() {
 
+
     return (
         <>
             <Canvas
                 style={{height: '50vh'}}
-                camera={{ position: [0, 0, 2] }}
+                camera={{ position: [-5, -1, 0] }}
             >
                 <Suspense fallback={null}>
                     <color attach="background" args={["black"]} />
