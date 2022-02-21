3ac7bb5b8b9897ab1caebda2576f834d1f47c43a
Darren Wong <email@darrenwong.net>
2022-02-21 22:41:21 +1100

    Somewhat working effect, particles don't relate to each other and appear to move randomly though

--- components/Ripple.js
+++ components/Ripple.js
@@ -3,13 +3,8 @@
 import React, { Suspense, useCallback, useMemo, useRef } from "react";
 import * as THREE from "three";
 import { applyProps, Canvas, useFrame } from "@react-three/fiber";
-import dot from "../public/assets/images/circle.png";
 import { useTexture, Loader, OrbitControls } from "@react-three/drei";
-
-// import dynamic from "next/dynamic";
-// const Points = dynamic(() => import("./RipplePoints"), {
-//   ssr: false,
-// });
+import noise, { perlin3 } from "./effects/noise";
 
 function Points() {
   const imgTex = useTexture("/assets/images/circle.png");
@@ -89,6 +84,103 @@ function Points() {
   );
 }
 
+function ProceduralPoints() {
+  const imgTex = useTexture("/assets/images/circle.png");
+  const bufferRef = useRef(undefined);
+  let t = 0;
+
+  const seed = Math.floor(Math.random() * 2 ** 16);
+  noise.seed(seed);
+
+  const sampleNoise = (x, y, z) => {
+    let scale = 1 / 8;
+    let octaves = 30;
+    let persistence = 0.6;
+    let lacunarity = 2;
+
+    let amp = 20;
+    let freq = 1;
+
+    let v = 0;
+    for (let i = 0; i < octaves; i++) {
+      v += amp * perlin3(x * freq * scale, y * freq * scale, z);
+      amp *= persistence;
+      freq *= lacunarity;
+    }
+
+    return v;
+  };
+
+  // Set initial positions
+  const count = 100;
+  const sep = 3; //
+  let positions = useMemo(() => {
+    let positions = [];
+
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = sampleNoise(x, t, z);
+
+        positions.push(x, y, z);
+      }
+    }
+
+    return new Float32Array(positions);
+  }, [count, sep, t]);
+
+  const lerp = useCallback((start, end, amt) => {
+    return (1 - amt) * start + amt * end;
+  }, []);
+
+  // Animate
+  useFrame(() => {
+    t += 0.01;
+    const positions = bufferRef.current.array;
+
+    let i = 0;
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        positions[i + 1] = lerp(
+          positions[i + 1],
+          sampleNoise(positions[i], t, positions[i + 2]),
+          0.1
+        ); // Change each y
+        // console.log(positions[i + 1]);
+        i += 3;
+      }
+    }
+
+    bufferRef.current.needsUpdate = true;
+  });
+
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          ref={bufferRef}
+          attachObject={["attributes", "position"]}
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0x00aaff}
+        size={0.5}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  );
+}
+
 export default function RippleScene(props) {
   return (
     <>
@@ -103,7 +195,7 @@ export default function RippleScene(props) {
         <OrbitControls />
         <color attach="background" args={["black"]} />
         <Suspense fallback={null}>
-          <Points />
+          <ProceduralPoints />
         </Suspense>
       </Canvas>
       <Loader />
